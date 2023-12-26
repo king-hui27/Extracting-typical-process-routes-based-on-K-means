7389ee4aadef945290eab18157b8bee38c3d8a86
PromiseHui <1397862207@qq.com>
2023-12-26 21:32:17 +0800

    仓库初始化

---
+++ processRoute-service/src/main/java/graduate/ecust/ProcessRouteMain.java
@@ -0,0 +1,14 @@
+package graduate.ecust;
+
+/**
+ * @author PromiseHui
+ * @version 1.0
+ * @project Default (Template) Project
+ * @description ${description}
+ * @date 2023/12/26 21:07:19
+ */
+public class ProcessRouteMain {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
